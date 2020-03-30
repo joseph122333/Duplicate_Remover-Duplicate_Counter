9b1c4e1bdb63bd1997f5f0241695ae57edd47a8c
jtlbo <jtlboca@gmail.com>
2020-03-29 20:05:31 -0400

    Assignment 4

---
+++ Lee_problem1/src/Lee_problem1/Application.java
@@ -0,0 +1,9 @@
+package Lee_problem1;
+
+public class Application {
+	public static void main(String[] args) {
+		DuplicateRemover duplicateRemover = new DuplicateRemover();
+		duplicateRemover.remove("problem1.txt");
+		duplicateRemover.write("unique_words.txt");
+}
+	}
